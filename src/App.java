68bfc261e16c728ebcf824d79f7023c9a3bf1f1c
UshcAriel <aushca@est.ups.edu.ec>
2026-07-15 09:00:44 -0500

    Listo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        runFibonaci();
+        FibonacciT(48);
     }
 
     private static void runFibonaci() {
@@ -22,5 +22,18 @@ public class App {
         }
         return getFibonacci(i - 1) + getFibonacci(i - 2);
     }
+    
+    private static long FibonacciT(int n) {
+        
+        if (n <= 1) return n;
+        long[] dp = new long[n+1];
+        dp[0] = 0; dp[1] = 1;
 
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i -1] + dp[i -2];
+        }
+        
+        System.err.println(dp[n]);
+        return dp[n];
+    }
 }
